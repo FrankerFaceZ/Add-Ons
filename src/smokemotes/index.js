e4a8b418558fc7d8fe59129841dbbfab1f20b04f
smokey019 <9442271+smokey019@users.noreply.github.com>
2019-12-20 00:01:52 -0700

    fixed personal emotes seperate gifs
    Fixed: Personal emotes should now work for everyone. Personal =/= Channel emotes.
    Changed: Static emotes and GIFs are now loaded seperately in case you decide you don't want GIFs or want to save data. Both are default on.
    Added: More settings.

--- src/smokemotes/index.js
+++ src/smokemotes/index.js
@@ -15,7 +15,18 @@ class SmokEmotes extends Addon {
 			ui: {
 				path: 'Add-Ons > smokEmotes >> Global Emotes',
 				title: 'Global Emotes',
-				description: 'Enable to show Smokeys global emoticons.',
+				description: 'Enable to show global emoticons.',
+				component: 'setting-check-box',
+			},
+		});
+
+		this.settings.add('smokemotes.global_gifs', {
+			default: true,
+
+			ui: {
+				path: 'Add-Ons > smokEmotes >> Global Emotes',
+				title: 'Global GIFs',
+				description: 'Enable to show global GIFs. (disable to save bandwidth)',
 				component: 'setting-check-box',
 			},
 		});
@@ -26,7 +37,7 @@ class SmokEmotes extends Addon {
 			ui: {
 				path: 'Add-Ons > smokEmotes >> Channel Emotes',
 				title: 'Channel Emotes',
-				description: 'Enable to show YOUR channel emoticons.',
+				description: 'Enable to show emoticons you\'ve uploaded.',
 				component: 'setting-check-box',
 			},
 		});
@@ -37,20 +48,21 @@ class SmokEmotes extends Addon {
 			ui: {
 				path: 'Add-Ons > smokEmotes >> Personal Emotes',
 				title: 'Personal Emotes',
-				description: 'Enable to show Personal emoticons (anywhere).',
+				description: 'Enable to show others\' Personal emoticons.',
 				component: 'setting-check-box',
 			},
 		});
 
-		this.chat.context.on('changed:smokemotes.global_emoticons', this.updateEmotes, this);
-		this.chat.context.on('changed:smokemotes.channel_emoticons', this.updateEmotes, this);
-		this.chat.context.on('changed:smokemotes.personal_emotes', this.updateEmotes, this);
+		this.chat.context.on('changed:smokemotes.global_emoticons', this.updateGlobalEmotes, this);
+		this.chat.context.on('changed:smokemotes.global_gifs', this.updateGlobalGIFs, this);
+		this.chat.context.on('changed:smokemotes.channel_emoticons', this.updateChannels, this);
+		this.chat.context.on('changed:smokemotes.personal_emotes', this.updatePersonalEmotes, this);
 	}
 
 	onEnable() {
 		this.log.debug('smokEmotes module was enabled successfully.');
 
-		//this.on('chat:receive-message', this.onReceiveMessage);
+		this.on('chat:receive-message', this.onReceiveMessage);
 
 		this.updateEmotes();
 
@@ -60,8 +72,8 @@ class SmokEmotes extends Addon {
 		const user = this.resolve('site').getUser();
 		if (user) {
 			const msg_user_id = msg.message.user.id;
-			if (user.id === msg_user_id) {
-				// this.socket.broadcastMe(msg.channel);
+			if (user.id != msg_user_id) {
+				this.updateOtherPersonalEmotes(msg);
 			}
 		}
 	}
@@ -75,7 +87,7 @@ class SmokEmotes extends Addon {
 			return;
 		}
 
-		const response = await fetch('https://bot.smokey.gg/api/emotes/?channel_id=global');
+		const response = await fetch('https://bot.smokey.gg/api/emotes/?channel_id=global&type=static');
 		if (response.ok) {
 			const emotes = await response.json();
 
@@ -135,6 +147,119 @@ class SmokEmotes extends Addon {
 		}
 	}
 
+	async updateGlobalGIFs(attempts = 0) {
+		const realID = 'addon--smokemotes--emotes-global-gifs';
+		this.emotes.removeDefaultSet('addon--smokemotes', realID);
+		this.emotes.unloadSet(realID);
+
+		if (!this.chat.context.get('smokemotes.global_gifs')) {
+			return;
+		}
+
+		const response = await fetch('https://bot.smokey.gg/api/emotes/?channel_id=global&type=gif');
+		if (response.ok) {
+			const emotes = await response.json();
+
+			const globalEmotes = [];
+
+			let i = emotes.length;
+			while (i--) {
+				const dataEmote = emotes[i];
+
+				const arbitraryEmote = /[^A-Za-z0-9]/.test(dataEmote.code);
+
+				const emote = {
+					id: dataEmote.id,
+					urls: {
+						1: dataEmote.images['1x'],
+						2: dataEmote.images['2x'],
+						4: dataEmote.images['3x']
+					},
+					name: dataEmote.code,
+					width: dataEmote.width,
+					height: dataEmote.height,
+					require_spaces: arbitraryEmote,
+				};
+
+				globalEmotes.push(emote);
+			}
+
+			const set = {
+				emoticons: globalEmotes,
+				title: 'Global GIFs',
+				source: 'smokEmotes',
+				icon: 'https://bot.smokey.gg/favicon.png',
+				_type: 1,
+			};
+
+			this.emotes.addDefaultSet('addon--smokemotes', realID, set);
+		} else {
+			if (response.status === 404) return;
+
+			const newAttempts = (attempts || 0) + 1;
+			if (newAttempts < 12) {
+				this.log.error('Failed to fetch global GIFs. Trying again in 5 seconds.');
+				setTimeout(this.updateGlobalGIFs.bind(this, newAttempts), 5000);
+			}
+		}
+	}
+
+	async updateOtherPersonalEmotes(msg) {
+
+		const _id_emotes = `addon--smokemotes--emotes-personal-${msg.message.user.id}`;
+		this.emotes.unloadSet(_id_emotes);
+
+		if (!this.chat.context.get('smokemotes.personal_emotes')) {
+			return;
+		}
+
+		const response = await fetch(`https://bot.smokey.gg/api/emotes/?channel_id=${msg.message.user.id}`);
+		if (response.ok) {
+			const emotes = await response.json();
+
+			const personalEmotes = [];
+
+			if (emotes.length == 0) { return; }
+
+			let i = emotes.length;
+			while (i--) {
+				const dataEmote = emotes[i];
+
+				const arbitraryEmote = /[^A-Za-z0-9]/.test(dataEmote.code);
+
+				const emote = {
+					id: dataEmote.id,
+					urls: {
+						1: dataEmote.images['1x'],
+						2: dataEmote.images['2x'],
+						4: dataEmote.images['3x']
+					},
+					name: dataEmote.code,
+					width: dataEmote.width,
+					height: dataEmote.height,
+					require_spaces: arbitraryEmote,
+					owner: {
+						display_name: dataEmote.user.displayName || '',
+						name: dataEmote.user.name || '',
+					},
+				};
+
+				personalEmotes.push(emote);
+			}
+
+			const set = {
+				emoticons: personalEmotes,
+				title: 'Personal Emotes',
+				source: 'smokEmotes',
+				icon: 'https://bot.smokey.gg/favicon.png',
+			};
+
+			this.emotes.loadSetData(_id_emotes, set, false);
+			this.chat.getUser(undefined, msg.message.user.login).addSet('addon--smokemotes', _id_emotes);
+
+		}
+	}
+
 	async updatePersonalEmotes(attempts = 0) {
 		const realID = 'addon--smokemotes--emotes-personal';
 		this.emotes.removeDefaultSet('addon--smokemotes', realID);
@@ -168,6 +293,10 @@ class SmokEmotes extends Addon {
 					width: dataEmote.width,
 					height: dataEmote.height,
 					require_spaces: arbitraryEmote,
+					/*owner: {
+						display_name: dataEmote.user.displayName || '',
+						name: dataEmote.user.name || '',
+					},*/
 				};
 
 				personalEmotes.push(emote);
@@ -200,13 +329,79 @@ class SmokEmotes extends Addon {
 		}
 	}
 
+	async updateChannelEmotes(room) {
+		const realID = `addon--smokemotes--channel-${room.id}`;
+		room.removeSet('addon--smokemotes', realID);
+		this.emotes.unloadSet(realID);
+
+		if (!this.chat.context.get('smokemotes.channel_emoticons')) {
+			return;
+		}
+
+		const response = await fetch(`https://bot.smokey.gg/api/emotes/?channel_id=${room.id}`);
+		if (response.ok) {
+			const emotes = await response.json();
+
+			const personalEmotes = [];
+
+			let i = emotes.length;
+			while (i--) {
+				const dataEmote = emotes[i];
+
+				const arbitraryEmote = /[^A-Za-z0-9]/.test(dataEmote.code);
+
+				const emote = {
+					id: dataEmote.id,
+					urls: {
+						1: dataEmote.images['1x'],
+						2: dataEmote.images['2x'],
+						4: dataEmote.images['3x']
+					},
+					name: dataEmote.code,
+					width: dataEmote.width,
+					height: dataEmote.height,
+					require_spaces: arbitraryEmote,
+					owner: {
+						display_name: dataEmote.user.displayName || '',
+						name: dataEmote.user.name || '',
+					},
+				};
+
+				personalEmotes.push(emote);
+			}
+
+			let setEmotes = [];
+			setEmotes = setEmotes.concat(personalEmotes);
+
+			if (setEmotes.length === 0) {
+				return;
+			}
+
+			const set = {
+				emoticons: setEmotes,
+				title: 'Channel Emotes',
+				source: 'smokEmotes',
+				icon: 'https://bot.smokey.gg/favicon.png',
+				_type: 1,
+			};
+
+			room.addSet('addon--smokemotes', realID, set);
+		} else {
+			this.log.error(`Failed to fetch channel (${room.id}) emotes.`);
+		}
+	}
+
+	updateChannels(){
+		for (const room of this.chat.iterateRooms()) {
+			if (room) this.updateChannelEmotes(room);
+		}
+	}
+
 	updateEmotes() {
 		this.updateGlobalEmotes();
+		this.updateGlobalGIFs();
 		this.updatePersonalEmotes();
-
-		/*for (const room of this.chat.iterateRooms()) {
-			if (room) this.updateChannel(room);
-		}*/
+		this.updateChannels();
 	}
 }
 
