02f903c17f53114edfeafb42f902dc06a244b23d
gr3ger <gr3ger@gmail.com>
2020-10-02 21:44:27 +0200

    Added user count below clear button

--- src/ModTools/views/clear-button.vue
+++ src/ModTools/views/clear-button.vue
@@ -1,7 +1,7 @@
 <template lang="html">
 	<section class="ffz--widget">
 		<button
-			class="tw-button tw-mg-x-1"
+			class="tw-button"
 			@click="clear"
 		>
 			<span class="tw-button__icon tw-button__icon--left">
@@ -11,6 +11,9 @@
 				{{ t('modtools.highlights-temp-users.clear', 'Clear highlights') }}
 			</span>
 		</button>
+		<p style="padding-top: 5px;">
+			{{ userCount() }} users in list
+		</p>
 	</section>
 </template>
 
@@ -20,16 +23,24 @@ export default {
 	props: ['item', 'context'],
 	data() {
 		return {
-			error_desc: null,
-			error: false,
-			message: null
 		}
 	},
+
+	computed:{
+
+	},
+
 	methods: {
 		clear(){
 			const settings = this.context.getFFZ().resolve('settings')
 			settings.provider.delete(Constants.HIGHLIGHT_USERS_KEY)
 			this.context.getFFZ().resolve('chat').emit('chat:update-lines')
+			this.$forceUpdate();
+		},
+
+		userCount(){
+			const settings = this.context.getFFZ().resolve('settings');
+			return settings.provider.get(Constants.HIGHLIGHT_USERS_KEY,[]).length;
 		},
 	},
 }
