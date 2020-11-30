22deb23c6ba403cf68f45e6b5f6a25e786779207
L3afMe <L3afMeAlon3@gmail.com>
2020-11-30 14:06:18 +1300

    Make Twitch Default preset actually make Twitch Default rather than my own theme.

--- src/brcm/preset/twitch_preset.js
+++ src/brcm/preset/twitch_preset.js
@@ -3,39 +3,38 @@ import {Preset} from './preset.js';
 export class TwitchDefaultPreset extends Preset {
 	constructor() {
 		super('twitch', 'Twitch (Default)',
-`#brcm-main-container .show {
-	background-color: #1A1A1A;
-	border:           1px solid #232223;
-	border-radius:    3px;
-	color:            #FFFFFF;
-	box-shadow:       0 0 3px rgb(0,0,0);
-	min-width:        150px;
+			`#brcm-main-container .show {
+		border:           1px solid #232223;
+		border-radius:    3px;
+		color:            #FFFFFF;
+		box-shadow:       0 0 3px rgb(0,0,0);
+		min-width:        150px;
 }
 
 #brcm-main-container .show li.separator-header {
-	background-color: #232223;
-	height:           1px;
+		background-color: #232223;
+		height:           1px;
 }
 
 #brcm-main-container .show li.separator-menu-item {
-	background-color: #232223;
-	height:           1px;
+		background-color: #232223;
+		height:           1px;
 }
 
 #brcm-main-container .show li.header {
-	background-color: #0D0D0D;
-	font-size:        15px;
-	padding:          4px 6px;
+		background-color: #18181B;
+		font-size:        15px;
+		padding:          2px 6px;
 }
 
 #brcm-main-container .show li:not(.separator-menu-item):not(.separator-header):not(.header) {
-	background-color: #1A1A1A;
-	font-size:        12px;
-	padding:          4px 6px;
+		background-color: #1E1E22;
+		font-size:        12px;
+		padding:          4px 6px;
 }
 
 #brcm-main-container .show li:not(.separator-menu-item):not(.separator-header):not(.header):hover {
-	background-color: #FFFFFF33;
+		background-color: #772CE8;
 }`);
 	}
 }
@@ -43,7 +42,7 @@ export class TwitchDefaultPreset extends Preset {
 export class TwitchFZZPreset extends Preset {
 	constructor() {
 		super('twitch_ffz', 'Twitch (FFZ)',
-`#brcm-main-container .show {
+			`#brcm-main-container .show {
     color:            var(--color-text-base) !important;
     background-color: var(--color-background-alt) !important;
     border:           var(--border-width-default) solid var(--color-border-base) !important;
