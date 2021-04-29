4ea0962e3be47d15b7606b611e4481c1a25c51af
SirStendec <sir@stendec.me>
2021-04-29 13:31:08 -0400

    Smokey's Utilities 1.1.0
    * Added: Setting for configuring which highlight reasons cause a message to become pinned.
    * Changed: Pinned messages now respect custom highlight colors.
    * Changed: Pinned messages now display a user's badges.
    * Fixed: Do not pin historical messages when loading a channel.
    * Fixed: Apply color adjustments when displaying pinned messages.

--- src/smokemotes/index.jsx
+++ src/smokemotes/index.jsx
@@ -34,6 +34,19 @@ class SmokeysUtils extends Addon {
 			},
 		});
 
+		this.settings.add('smokemotes.pinned_reasons', {
+			default: ['mention'],
+			type: 'basic_array_merge',
+			ui: {
+				path: "Add-Ons > Smokey's Utilities >> Pinned Mentions",
+				title: 'Pin Reasons',
+				description: 'Pin messages when a chat line is highlighted for one of the selected reasons.',
+				component: 'setting-select-box',
+				multiple: true,
+				data: () => this.chat.getHighlightReasons()
+			}
+		});
+
 		this.settings.add('smokemotes.pinned_messages_remove', {
 			default: true,
 
@@ -53,8 +66,7 @@ class SmokeysUtils extends Addon {
 				sort: 1,
 				path: "Add-Ons > Smokey's Utilities >> Pinned Mentions",
 				title: 'Maximum Pinned Messages',
-				description:
-          'Maximum amount of pinned messages allowed to be in the pinned mentions section.',
+				description: 'Maximum amount of pinned messages allowed to be in the pinned mentions section.',
 				component: 'setting-text-box',
 				process(val) {
 					val = parseFloat(val, 10);
@@ -72,8 +84,7 @@ class SmokeysUtils extends Addon {
 				sort: 2,
 				path: "Add-Ons > Smokey's Utilities >> Pinned Mentions",
 				title: 'Auto Removal Timer',
-				description:
-          'Time in seconds to auto remove pinned messages. 0 To Disable.',
+				description: 'Time in seconds to auto remove pinned messages. 0 To Disable.',
 				component: 'setting-text-box',
 				process(val) {
 					val = parseFloat(val, 10);
@@ -146,8 +157,7 @@ class SmokeysUtils extends Addon {
 				sort: 0,
 				path: "Add-Ons > Smokey's Utilities >> Channel",
 				title: 'Auto Point Claimer',
-				description:
-          'Enable to automatically obtain channel points. After disabling, you must refresh to completely disable.',
+				description: 'Enable to automatically obtain channel points. After disabling, you must refresh to completely disable.',
 				component: 'setting-check-box',
 			},
 		});
@@ -159,8 +169,7 @@ class SmokeysUtils extends Addon {
 				sort: 0,
 				path: "Add-Ons > Smokey's Utilities >> Following",
 				title: 'Auto go to Live channels on your Following Directory',
-				description:
-          'Enable to automatically go to the Live channel section on a fresh load of the following directory.',
+				description: 'Enable to automatically go to the Live channel section on a fresh load of the following directory.',
 				component: 'setting-check-box',
 			},
 		});
@@ -199,7 +208,7 @@ class SmokeysUtils extends Addon {
 
 		this.ViewerCard = this.fine.define(
 			'chat-viewer-card',
-			(n) => n.trackViewerCardOpen && n.onWhisperButtonClick
+			n => n.trackViewerCardOpen && n.onWhisperButtonClick
 		);
 
 		this.style_link = null;
@@ -214,107 +223,33 @@ class SmokeysUtils extends Addon {
 			priority: 0,
 
 			process(tokens, msg) {
-				if ( ! msg.mentioned || msg.smokey_pinned || ! msg.highlights?.has?.('mention') || ! this.settings.get('smokemotes.pinned_mentions') )
+				if ( msg.smokey_pinned )
 					return tokens;
 
 				msg.smokey_pinned = true;
+				if ( ! msg.mentioned || msg.isHistorical || ! t.settings.get('smokemotes.pinned_mentions') )
+					return tokens;
 
-				const pinned_border = this.settings.get(
-					'smokemotes.pinned_border'
-				);
-				let chat_list;
 				try {
-					chat_list = t.site_chat.ChatContainer.first.state.chatListElement;
-				} catch {
-					this.log.debug('error getting chat_list');
-				}
-				if (chat_list) {
-					let chat_log;
-					try {
-						chat_log = chat_list.querySelector('[role="log"]');
-					} catch {
-						this.log.debug('error getting chat_log');
-					}
-					if (chat_log) {
-						const pinned_background = this.settings.get(
-							'smokemotes.pinned_bg'
-						);
-						const pinned_font = this.settings.get(
-							'smokemotes.pinned_font_color'
-						);
-						const pinned_log =
-							document.getElementById('pinned_log') ||
-							document.createElement('div');
-						pinned_log.setAttribute(
-							'style',
-							`position: absolute; color: ${pinned_font}; background-color: ${pinned_background}; z-index: 1000; width: 100%;`
-						);
-						pinned_log.id = 'pinned_log';
-						pinned_log.classList.add('pinned-highlight-log');
-						chat_log.parentNode.prepend(pinned_log);
-						if (
-							pinned_log.childNodes.length >=
-							this.settings.get('smokemotes.pinned_messages_max')
-						) {
-							if (
-								this.settings.get('smokemotes.pinned_messages_remove')
-							) {
-								pinned_log.childNodes[0].remove();
-							} else {
-								return tokens;
-							}
-						}
-						requestAnimationFrame(() => {
-							const pin_id = Date.now() + Math.floor(Math.random() * 101),
-								tokens = msg.ffz_tokens || null;
-							let line;
-							const close_fn = () => {
-								line && line.remove();
-								line = null;
-							};
-							line = (<div
-								id={pin_id}
-								class="chat-line__message"
-								style={`border: 1px solid ${pinned_border} !important; border-top: none !important`}
-							>
-								<div
-									style="width: 14px; cursor: pointer; top: 5px; right: 5px; position: absolute"
-									// eslint-disable-next-line react/jsx-no-bind
-									onclick={close_fn}
-								>
-									<figure class="ffz-i-cancel" />
-								</div>
-								<span class="chat-line__timestamp">
-									{ this.formatTime(msg.timestamp) }
-								</span>
-								<span class="chat-line__username notranslate" role="button">
-									<span class="chat-author__display-name">
-										<a rel="noopener noreferrer" target="_blank" href={`https://twitch.tv/${msg.user.login}`} style={{color: msg.user.color}}>
-											{ msg.user.displayName || msg.user.login }
-										</a>
-									</span>
-								</span>
-								{': '}
-								{tokens ? this.renderTokens(tokens) : msg.message}
-							</div>);
-							pinned_log.appendChild(line);
-							if (document.hidden)
-								document.querySelector(
-									'link[rel="icon"]'
-								).href = this.notify_icon;
-							if (this.settings.get('smokemotes.pinned_timer') != 0) {
-								setTimeout(close_fn, this.settings.get('smokemotes.pinned_timer') * 1000);
-							}
-						});
-					}
+					t.handlePin(msg);
+				} catch(err) {
+					t.log.error('Error handling pin for message.', err);
 				}
 
 				return tokens;
-			},
+			}
 		};
 
-		if (this.settings.get('smokemotes.pinned_mentions')){
+		if ( this.settings.get('smokemotes.pinned_mentions') ) {
+			this.pinned_registered = true;
 			this.chat.addTokenizer(Pinned_Mentions);
+		} else {
+			this.settings.on(':changed:smokemotes.pinned_mentions', () => {
+				if ( ! this.pinned_registered && this.settings.get('smokemotes.pinned_mentions') ) {
+					this.pinned_registered = true;
+					this.chat.addTokenizer(Pinned_Mentions);
+				}
+			});
 		}
 	}
 
@@ -331,6 +266,148 @@ class SmokeysUtils extends Addon {
 		this.ViewerCard.on('update', this.updateCard, this);
 		this.ViewerCard.on('unmount', this.unmountCard, this);
 	}
+
+	/**
+	 * Handle new messages, see if they should be pinned and pin them.
+	 */
+
+	handlePin(msg) {
+		const highlights = msg.highlights;
+		if ( ! highlights?.size )
+			return;
+
+		const types = this.settings.get('smokemotes.pinned_reasons');
+		let matched = false;
+		for(const type of types)
+			if ( highlights.has(type) ) {
+				matched = true;
+				break;
+			}
+
+		if ( ! matched )
+			return;
+
+		let pinned_log = document.querySelector('#smokey_pinned_log');
+		if ( ! pinned_log ) {
+			const container = this.site_chat.ChatContainer.first,
+				el = container?.state?.chatListElement,
+				log = el && el.querySelector('[role="log"]');
+
+			if ( ! log )
+				return;
+
+			pinned_log = createElement('div', {
+				id: 'smokey_pinned_log',
+				class: 'pinned-highlight-log tw-absolute tw-top-0 tw-full-width tw-z-above tw-c-background-base'
+			});
+
+			log.parentNode.prepend(pinned_log);
+		}
+
+		pinned_log.style.color = this.settings.get('smokemotes.pinned_font_color');
+
+		if ( pinned_log.childNodes.length >= this.settings.get('smokemotes.pinned_messages_max') ) {
+			if ( this.settings.get('smokemotes.pinned_messages_remove') )
+				pinned_log.childNodes[0].remove();
+			else
+				return;
+		}
+
+		let bg_color = msg.mention_color;
+		if ( bg_color )
+			bg_color = this.site_chat.inverse_colors.process(bg_color);
+		if ( ! bg_color )
+			bg_color = this.site_chat.inverse_colors.process(this.settings.get('smokemotes.pinned_bg'));
+
+		let color = msg.user.color;
+		if ( color )
+			color = this.site_chat.colors.process(color);
+
+		const msg_types = this.site_chat.message_types || {},
+			is_action = msg.messageType === msg_types.Action,
+			action_style = is_action ? this.chat.context.get('chat.me-style') : 0,
+			action_italic = action_style >= 2,
+			action_color = action_style === 1 || action_style === 3;
+
+		requestAnimationFrame(() => {
+			const pin_id = Date.now() + Math.floor(Math.random() * 101),
+				tokens = msg.ffz_tokens || null,
+				pinned_border = this.settings.get('smokemotes.pinned_border');
+
+			let line, timer;
+			const close_fn = () => {
+				timer && clearTimeout(timer);
+				line && line.remove();
+				line = timer = null;
+			};
+
+			let room = msg.roomLogin ? msg.roomLogin : msg.channel ? msg.channel.slice(1) : undefined,
+				room_id = msg.roomId;
+
+			if ( ! room && room_id ) {
+				const r = this.chat.getRoom(room_id, null, true);
+				if ( r && r.login )
+					room = msg.roomLogin = r.login;
+			}
+
+			if ( ! room_id && room ) {
+				const r = this.chat.getRoom(null, room, true);
+				if ( r && r.id )
+					room_id = msg.roomId = r.id;
+			}
+
+			line = (<div
+				id={pin_id}
+				class={`chat-line__message${bg_color ? ' ffz-custom-color' : ''}`}
+				style={`border: 1px solid ${pinned_border} !important; border-top: none !important;${bg_color ? `background-color:${bg_color};` : ''}`}
+				data-room={room}
+				data-room-id={room_id}
+				data-user={msg.user.login}
+				data-user-id={msg.user.id}
+			>
+				<div
+					style="width: 14px; cursor: pointer; top: 5px; right: 5px; position: absolute"
+					// eslint-disable-next-line react/jsx-no-bind
+					onclick={close_fn}
+				>
+					<figure class="ffz-i-cancel" />
+				</div>
+				<span class="chat-line__timestamp">
+					{ this.chat.formatTime(msg.timestamp) }
+				</span>
+				<span class="chat-line__message--badges">
+					{ this.chat.badges.render(msg, createElement) }
+				</span>
+				<span class="chat-line__username notranslate" role="button">
+					<span class="chat-author__display-name">
+						<a rel="noopener noreferrer" target="_blank" href={`https://twitch.tv/${msg.user.login}`} style={{color}}>
+							{ msg.user.displayName || msg.user.login }
+						</a>
+					</span>
+				</span>
+				{is_action ? ' ' : ': '}
+				<span
+					class={`message ${action_italic ? 'chat-line__message-body--italicized' : ''}`}
+					style={{color: action_color ? color : null}}
+				>
+					{tokens ? this.chat.renderTokens(tokens) : msg.message}
+				</span>
+			</div>);
+
+			pinned_log.appendChild(line);
+
+			if (document.hidden)
+				document.querySelector(
+					'link[rel="icon"]'
+				).href = this.notify_icon;
+
+			const timeout = this.settings.get('smokemotes.pinned_timer');
+			if ( timeout > 0 ) {
+				timer = setTimeout(close_fn, timeout * 1000);
+			}
+		});
+	}
+
 
 	/**
 	 * Automatically Claim Channel Points Observer
