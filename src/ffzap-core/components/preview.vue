eb4c08d6e79b1a0c87eac70519116370bc5b9e23
SirStendec <sir@stendec.me>
2021-05-17 18:00:25 -0400

    FFZ:AP Core 3.3.6
    * Fixed: Layout issues caused by updates to Twitch CSS classes.

--- src/ffzap-core/components/preview.vue
+++ src/ffzap-core/components/preview.vue
@@ -1,11 +1,11 @@
 <template lang="html">
 	<div>
 		<button
-			class="tw-mg-l-05 tw-mg-y-05 tw-button tw-button--text tw-tooltip__container tw-relative"
+			class="tw-mg-l-05 tw-mg-y-05 tw-button tw-button--text ffz-il-tooltip__container tw-relative"
 			@click="click"
 		>
 			<span class="tw-button__text ffz-i-play" />
-			<div class="tw-tooltip tw-tooltip--down tw-tooltip--align-right">
+			<div class="ffz-il-tooltip ffz-il-tooltip--down ffz-il-tooltip--align-right">
 				{{ t('setting.preview', 'Preview') }}
 			</div>
 		</button>
