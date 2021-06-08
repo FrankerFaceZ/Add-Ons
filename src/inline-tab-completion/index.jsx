7d72d3b342d68c1c6b4a78b93cb4ad1ffa34dfec
SirStendec <sir@stendec.me>
2021-06-08 19:15:37 -0400

    Inline Tab-Completion 1.1.0
    * Added: Optional tooltip when performing tab-completion with a preview of whatever emote you are inserting.
    * Added: Setting to disable the mandatory `@` prefix on mentions.

--- src/inline-tab-completion/index.jsx
+++ src/inline-tab-completion/index.jsx
@@ -1,6 +1,11 @@
 'use strict';
 
+const { createElement, setChildren } = FrankerFaceZ.utilities.dom;
 const { KEYS } = FrankerFaceZ.utilities.constants;
+const { Tooltip } = FrankerFaceZ.utilities.tooltip;
+
+import STYLE_URL from './styles.scss';
+
 const Installed = Symbol('KeyHandler');
 const Focus = Symbol('FocusHandler');
 
@@ -11,11 +16,27 @@ class InlineTab extends Addon {
 		this.inject('i18n');
 		this.inject('settings');
 		this.inject('site.chat.input');
+		this.inject('tooltips')
+
+		this.settings.add('addon.inlinetab.tips', {
+			default: true,
+			ui: {
+				path: 'Add-Ons > Inline Tab >> Appearance',
+				title: 'Display a tooltip when using tab-completion.',
+				component: 'setting-check-box'
+			},
+			changed: val => {
+				if ( val )
+					this.createTooltip();
+				else
+					this.destroyTooltip();
+			}
+		});
 
 		this.settings.add('addon.inlinetab.no_commands', {
 			default: true,
 			ui: {
-				path: 'Add-Ons > Inline Tab >> General',
+				path: 'Add-Ons > Inline Tab >> Content @{"description": "Please note that the built-in Twitch tab-completion will be used for content that is not included in inline tab-completion."}',
 				title: 'Do not include commands in inline tab-completion.',
 				component: 'setting-check-box'
 			},
@@ -25,11 +46,20 @@ class InlineTab extends Addon {
 		this.settings.add('addon.inlinetab.no_mentions', {
 			default: false,
 			ui: {
-				path: 'Add-Ons > Inline Tab >> General @{"description": "Please note that the built-in Twitch tab-completion will be used for content that is not included in inline tab-completion."}',
+				path: 'Add-Ons > Inline Tab >> Content',
 				title: 'Do not include users in inline tab-completion.',
 				component: 'setting-check-box'
 			},
 			changed: () => this.updateProviders()
+		});
+
+		this.settings.add('addon.inlinetab.mention_prefix', {
+			default: true,
+			ui: {
+				path: 'Add-Ons > Inline Tab >> General',
+				title: 'Always prefix mentions with `@` signs.',
+				component: 'setting-check-box'
+			}
 		});
 	}
 
@@ -40,6 +70,9 @@ class InlineTab extends Addon {
 			for(const inst of instances)
 				this.installInputHandler(inst);
 		});
+
+		this.createStyle();
+		this.createTooltip();
 	}
 
 	onDisable() {
@@ -48,6 +81,145 @@ class InlineTab extends Addon {
 		for(const inst of this.input.ChatInput.instances) {
 			this.uninstallInputHandler(inst);
 		}
+
+		this.destroyStyle();
+		this.destroyTooltip();
+	}
+
+	destroyStyle() {
+		if ( this.style ) {
+			this.style.remove();
+			this.style = null;
+		}
+	}
+
+	createStyle() {
+		if ( this.style )
+			return;
+
+		this.style = createElement('link', {
+			href: STYLE_URL,
+			rel: 'stylesheet',
+			type: 'text/css',
+			crossOrigin: 'anonymous'
+		});
+
+		document.head.appendChild(this.style);
+	}
+
+	destroyTooltip() {
+		if ( this.tt ) {
+			this.tt.destroy();
+			this.tt = null;
+		}
+	}
+
+	createTooltip() {
+		if ( this.tt )
+			this.destroyTooltip();
+
+		if ( ! this.settings.get('addon.inlinetab.tips') )
+			return;
+
+		const container = this.tooltips.getRoot();
+		if ( ! container )
+			return;
+
+		this.tt = new Tooltip(
+			container, [], {
+				logger: this.log,
+				i18n: this.i18n,
+				manual: true,
+				live: false,
+				html: true,
+
+				tooltipClass: 'ffz-action-balloon ffz-balloon tw-block tw-border tw-elevation-1 tw-border-radius-small tw-c-background-base',
+				arrowClass: 'ffz-balloon__tail tw-overflow-hidden tw-absolute',
+				arrowInner: 'ffz-balloon__tail-symbol tw-border-t tw-border-r tw-border-b tw-border-l tw-border-radius-small tw-c-background-base tw-absolute',
+				innerClass: 'tw-pd-05',
+
+				popper: {
+					placement: 'top-start',
+					modifiers: {
+						preventOverflow: {
+							boundariesElement: container
+						},
+						flip: {
+							behavior: ['top', 'bottom', 'left', 'right']
+						}
+					}
+				},
+
+				content: (el, tip) => this.renderTooltip(el._ffz_inst, tip)
+			}
+		);
+	}
+
+	renderTooltip(inst) {
+		if ( ! inst )
+			return null;
+
+		const suggestions = inst.ffztc_suggestions,
+			pos = inst.ffztc_position,
+			current = suggestions?.[pos];
+
+		if ( ! current || ! suggestions )
+			return null;
+
+		let type, text = current.text, letter;
+
+		if ( current.emoji ) {
+			type = this.i18n.t('addon.inlinetab.type.emoji', 'Emoji');
+			text = `:${current.emoji.names[0]}:`;
+
+		} else if ( current.emote ) {
+			type = this.i18n.t('addon.inlinetab.type.emote', 'Emote');
+
+		} else {
+			if ( current.source === 'command' ) {
+				letter = '/';
+				type = this.i18n.t('addon.inlinetab.type.command', 'Command');
+			} else if ( current.source === 'mention' ) {
+				letter = '@';
+				type = this.i18n.t('addon.inlinetab.type.mention', 'Mention');
+			} else
+				type = null;
+
+			const s = current.sel;
+			if ( s )
+				text = [
+					// eslint-disable-next-line react/jsx-key
+					<span class="tw-strong">{text.slice(0, s[0])}</span>,
+					// eslint-disable-next-line react/jsx-key
+					<span class="tw-c-text-alt">{text.slice(s[0], s[0] + s[1])}</span>,
+					// eslint-disable-next-line react/jsx-key
+					<span class="tw-strong">{text.slice(s[0] + s[1])}</span>
+				];
+		}
+
+		return (<div class="ffztc-preview tw-flex">
+			<div class="tw-relative tw-flex-shrink-0 ffztc-preview__image tw-flex tw-align-items-center tw-justify-content-center tw-mg-r-05">
+				{letter && ! current.src && <figure class="tw-c-text-alt-2 tw-strong tw-font-size-3">{letter}</figure>}
+				{current.src && <img srcSet={current.src} />}
+				{current.fav && <figure class="ffz--favorite ffz-i-star" />}
+			</div>
+			<div class="tw-flex-grow-1 tw-overflow-hidden">
+				<div class="tw-ellipsis">
+					{ Array.isArray(text) ? text : <span class="tw-strong">{text}</span> }
+				</div>
+				<div class="tw-flex tw-border-t tw-c-text-alt tw-font-size-8">
+					<div class="tw-flex-grow-1">
+						{ type }
+					</div>
+					<div class="tw-flex-shrink-0 tw-mg-l-1">
+						{ this.i18n.t('addon.inlinetab.item', '{pos,number} of {total,number}', {
+							pos: pos + 1,
+							total: suggestions.length
+						})}
+					</div>
+				</div>
+			</div>
+		</div>);
 	}
 
 	updateProviders(inst) {
@@ -90,10 +262,9 @@ class InlineTab extends Addon {
 		inst[Installed] = null;
 
 		inst.chatInputRef.removeEventListener('focus', inst[Focus]);
+		inst.chatInputRef.removeEventListener('blur', inst[Focus]);
 
-		inst.ffztc_position = -1;
-		inst.ffztc_parts = inst.ffztc_suggestions = null;
-
+		this.clearAutocomplete(inst);
 		this.updateProviders(inst);
 	}
 
@@ -104,8 +275,9 @@ class InlineTab extends Addon {
 		const handler = inst[Installed] = this.handleKey.bind(this, inst);
 		inst.chatInputRef.addEventListener('keydown', handler);
 
-		const focus = inst[Focus] = this.handleFocus.bind(this, inst);
+		const focus = inst[Focus] = this.clearAutocomplete.bind(this, inst);
 		inst.chatInputRef.addEventListener('focus', focus);
+		inst.chatInputRef.addEventListener('blur', focus);
 
 		inst.ffztc_position = -1;
 		inst.ffztc_parts = inst.ffztc_suggestions = null;
@@ -113,9 +285,14 @@ class InlineTab extends Addon {
 		this.updateProviders(inst);
 	}
 
-	handleFocus(inst) {
+	clearAutocomplete(inst) {
 		inst.ffztc_position = -1;
 		inst.ffztc_parts = inst.ffztc_suggestions = null;
+
+		if ( this.tt )
+			this.tt._exit(inst.chatInputRef);
+
+		inst.chatInputRef._ffz_inst = null;
 	}
 
 	handleKey(inst, event) {
@@ -177,7 +354,7 @@ class InlineTab extends Addon {
 
 						// Only include an automatic prefix if not
 						// entering a command.
-						if ( inst.ffztc_parts[0][0] !== '/' || token[0] === '@' )
+						if ( (this.settings.get('addon.inlinetab.mention_prefix') && inst.ffztc_parts[0][0] !== '/') || token[0] === '@' )
 							prefix = '@';
 
 						// The mention handler doesn't allow input to start with
@@ -201,8 +378,13 @@ class InlineTab extends Addon {
 								idx += prefix.length;
 
 							suggestions.push({
-								t: prefix + text,
-								s: idx === -1 ? null : [idx, length]
+								source: type,
+								text: prefix + text,
+								sel: idx === -1 ? null : [idx, length],
+								fav: item.favorite,
+								emoji: item.emoji,
+								emote: item.emote,
+								src: item.emote?.srcSet || item.srcSet
 							});
 						}
 				}
@@ -229,16 +411,26 @@ class InlineTab extends Addon {
 					let caret = parts[0].length;
 					let end;
 
-					if ( suggestion.s ) {
-						caret += suggestion.s[0];
-						end = caret + suggestion.s[1];
+					if ( suggestion.sel ) {
+						caret += suggestion.sel[0];
+						end = caret + suggestion.sel[1];
 					} else {
-						caret += suggestion.t.length + (empty ? 1 : 0);
+						caret += suggestion.text.length + (empty ? 1 : 0);
 						end = caret;
 					}
 
-					inst.autocompleteInputRef.setValue(parts[0] + suggestion.t + (empty && ! suggestion.s ? ' ' :  parts[2]));
+					inst.autocompleteInputRef.setValue(parts[0] + suggestion.text + (empty && ! suggestion.sel ? ' ' :  parts[2]));
 					inst.chatInputRef.setSelectionRange(caret, end);
+
+					if ( this.tt ) {
+						inst.chatInputRef._ffz_inst = inst;
+						const tip = inst.chatInputRef[this.tt._accessor];
+						if ( tip ) {
+							setChildren(tip.element, this.renderTooltip(inst, tip));
+							tip.update();
+						} else
+							this.tt._enter(inst.chatInputRef);
+					}
 				}
 			}
 
@@ -249,8 +441,7 @@ class InlineTab extends Addon {
 			// Delete our state.
 			inst.autocompleteInputRef.setValue(inst.ffztc_parts.join(''));
 			inst.chatInputRef.setSelectionRange(inst.ffztc_caret, inst.ffztc_caret);
-			inst.ffztc_position = -1;
-			inst.ffztc_parts = inst.ffztc_suggestions = null;
+			this.clearAutocomplete(inst);
 
 			event.preventDefault();
 			event.stopPropagation();
@@ -261,8 +452,7 @@ class InlineTab extends Addon {
 		if ( code !== KEYS.Shift ) {
 			// Any other key press means we accept tab-completion
 			// and move on. Delete our state.
-			inst.ffztc_position = -1;
-			inst.ffztc_parts = inst.ffztc_suggestions = null;
+			this.clearAutocomplete(inst);
 		}
 	}
 }
