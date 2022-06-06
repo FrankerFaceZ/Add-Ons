e2652a83261f0898986febd337fd388e876eec6a
kevuwk <kevuwk@gmail.com>
2022-06-06 19:21:37 +0100

    Chants V1
    Chants V1

---
+++ src/chants/chants.jsx
@@ -0,0 +1,95 @@
+let num = 0;
+let timer;
+let tCurrent = 0;
+
+export function chantCooldown() {
+  let progress = document.getElementById("chant_progress");
+  num = 100;
+  tCurrent = Date.now() + 25000;
+
+  timer = setTimeout(function() { IncrementBar() },24);
+}
+
+export function IncrementBar ()
+{
+  let progress = document.getElementById("chant_progress");
+  if(num.toFixed(2) <= 0.1) {
+	removeChantButton();
+	return;
+  }
+  let tNow = Date.now();
+  let iDifference = tCurrent - tNow;
+  let iDelay = iDifference / (num*10);
+  iDelay = Math.floor(iDelay)
+  timer = setTimeout(function() { IncrementBar() },iDelay);
+  progress.style.width = num + "%";
+  num -= 0.1;
+}
+
+
+export function removeChantButton() {
+  document.getElementById("chant_popup").remove();
+}
+
+export function addChantButton( callback, username, message, channel) {
+  let container = document.createElement("section");
+  container.id = "chant_popup";
+
+  let title = document.createElement("h4");
+  title.innerText = username + " started a chant";
+  container.appendChild(title);
+
+  let xButtonSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  xButtonSvg.id = "chant_close_button";
+  xButtonSvg.setAttribute("version", "1.1");
+  xButtonSvg.setAttribute("viewBox", "0 0 20 20");
+  xButtonSvg.setAttribute("x", "0px");
+  xButtonSvg.setAttribute("y", "0px");
+  xButtonSvg.addEventListener("click", function() {
+	clearTimeout(timer);
+    removeChantButton();
+  });
+  container.appendChild(xButtonSvg);
+
+  let xButtonPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  xButtonPath.setAttribute("d", "M8.5 10L4 5.5 5.5 4 10 8.5 14.5 4 16 5.5 11.5 10l4.5 4.5-1.5 1.5-4.5-4.5L5.5 16 4 14.5 8.5 10z");
+  xButtonSvg.appendChild(xButtonPath);
+
+  let messageParagraph = document.createElement("p");
+  messageParagraph.id = "chant_message";
+  messageParagraph.innerText = message;
+  container.appendChild(messageParagraph);
+
+  let chantButton = document.createElement("button");
+  chantButton.id = "chant_button";
+  chantButton.type = "button";
+  chantButton.name = "button";
+  chantButton.innerText = "Chant";
+  chantButton.addEventListener("click", function() {
+	clearTimeout(timer);
+    removeChantButton();
+	callback.resolve('site.chat').sendMessage(channel, message);
+  });
+  container.appendChild(chantButton);
+
+  let barContainer = document.createElement("span");
+  barContainer.id = "chant_progressbar";
+  container.appendChild(barContainer);
+
+  let barProgress = document.createElement("span");
+  barProgress.id = "chant_progress";
+  barContainer.appendChild(barProgress);
+
+  return container
+}
+
+export function sendAnonMessage(message) {
+	let div = document.createElement('div');
+	div.classList.add('chat-line__status');
+	
+	let span = document.createElement('span');
+	span.innerText = message;
+	div.appendChild(span);
+
+	document.getElementsByClassName('chat-scrollable-area__message-container')[0].appendChild(div);
+}
---
+++ src/chants/index.js
@@ -0,0 +1,145 @@
+// A lot of helpful stuff is tucked away in FrankerFaceZ's utilities.
+// You're recommended to go over it to see what's available before
+// pulling in external dependencies.
+import * as Chants from './Chants.jsx'
+import STYLE_URL from './styles.scss'
+const createElement = FrankerFaceZ.utilities.dom.createElement;
+
+// Add-Ons should contain a class that extends Addon.
+export class TwitchChants extends Addon {
+	constructor(...args) {
+		super(...args);
+		this.inject('chat');
+		this.inject('site');
+		this.injectAs('site_chat', 'site.chat');
+		let current_chantmessage = "";
+		let current_chanttime = 0; 
+		let chant_delay = 25000;
+		let user = this.site.getUser();
+		
+
+		// Within the constructor, modules can inject dependencies.
+		// A module will not be enabled until all its dependencies
+		// have been loaded and enabled.
+
+		// In this case, we're depending on the FFZ module "metadata".
+		this.inject('metadata');
+
+		// All add-ons, by default, depend on the "settings" and
+		// "i18n" modules.
+
+		// If you need to ensure a dependency is loaded before
+		// onLoad is called, you need to create a list on the
+		// Addon here with the names of the dependencies.
+		this.load_requires = ['metadata'];
+		
+		const ChantsProcessor = {
+			process(tokens, msg) {
+				if (!msg.isHistorical) {
+					if (msg.badges.moderator || msg.badges.broadcaster) {
+						if (msg.message.split(" ")[0] == "!chant") {
+							let current_chantmessage = msg.message.split("!chant")[1].trim();
+							if(current_chantmessage == "") {
+								this.log.info("no chant message included");
+								if (user.login == msg.user.login) { Chants.sendAnonMessage("Correct usage of the command is: !chant <message>"); }
+							}
+							else {
+								let message_time = parseInt(msg.timestamp);
+								if (message_time > current_chanttime + chant_delay) {
+									let box = Chants.addChantButton( this, msg.user.login, current_chantmessage, msg.roomLogin);
+									document.getElementsByClassName("chat-input")[0].children[0].appendChild(box);
+									Chants.chantCooldown();
+									current_chanttime = message_time;
+								}
+								else
+								{
+									this.log.info ("chant on cooldown");
+									if (user.login == msg.user.login) { Chants.sendAnonMessage("A chant is already running."); }
+								}
+							}
+						}
+					}
+				}
+				return tokens; 
+			}
+		}
+		this.chat.addTokenizer(ChantsProcessor);
+	}
+	
+	// onLoad is called when the module is being loaded, prior to
+	// being enabled. If this method returns a Promise, FFZ will
+	// wait for the promise to resolve before considering the
+	// module as loaded.
+	async onLoad() {
+		// We don't actually need to do anything here, but if
+		// we did, we'd already have guaranteed access to the
+		// metadata module because of the earlier "load_requires"
+	}
+
+	// onEnable is called when the module is ready and should be
+	// enabled. This happens when an Addon is registered, when a
+	// module instance's ".enable()" method is called, or when a
+	// module that depends on this module is enabled. If this
+	// method returns a Promise, FFZ will wait for the promise
+	// to resolve before considering the module as enabled.
+	onEnable() {
+		// All modules have a logger instance by default. Module
+		// loggers prepend the name of the module to output.
+		this.log.info('Chants Extension Enabled');
+
+		
+		this.createStyle();
+
+	}
+
+	// onDisable is called when the module should be disabled.
+	// This happens when a module instance's ".disable()" method
+	// is explicitly called, when a module instance's ".unload()"
+	// method is explicitly called, or when a user clicks a module's
+	// "Disable" button in the Add-On listing. If a module cannot
+	// be fully disabled, this method should be omitted. If this
+	// method is omitted, attempting to disable the module will
+	// fail. The Add-Ons UI will tell users they must refresh
+	// the pages for changes to apply. If this method returns a
+	// Promise, FFZ will wait for the promise to resolve before
+	// considering the module as disabled.
+	onDisable() {
+		this.destroyStyle();
+	}
+
+	// onUnload is called when the module should be unloaded.
+	// This happens when a module instance's ".unload()" method
+	// is explicitly called. Just disabling a module will not
+	// cause it to be unloaded. If a module cannot be unloaded
+	// meaningfully, this method should be omitted. If this method
+	// is omitted, attempting to unload the module will fail.
+	async onUnload() {
+		/* no-op */
+	}
+	
+	destroyStyle() {
+		if ( this.style ) {
+			this.style.remove();
+			this.style = null;
+		}
+	}
+
+	createStyle() {
+		if ( this.style )
+			return;
+
+		this.style = createElement('link', {
+			href: STYLE_URL,
+			rel: 'stylesheet',
+			type: 'text/css',
+			crossOrigin: 'anonymous'
+		});
+
+		document.head.appendChild(this.style);
+	}
+}
+
+// Addons should register themselves. Doing so adds
+// them to FFZ's map of known modules, and also attempts
+// to enable the module.
+TwitchChants.register();
