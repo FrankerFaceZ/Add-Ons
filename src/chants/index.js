b6e2964680a4634948cd9b9fe510ddff3b5099a9
kevuwk <kevuwk@gmail.com>
2022-06-11 13:06:28 +0100

    Addition of /chant
    Added /chant command

--- src/chants/index.js
+++ src/chants/index.js
@@ -4,6 +4,9 @@
 import * as Chants from './Chants.jsx'
 import STYLE_URL from './styles.scss'
 const createElement = FrankerFaceZ.utilities.dom.createElement;
+let current_chantmessage = "";
+let current_chanttime = 0; 
+let chant_delay = 25000;
 
 // Add-Ons should contain a class that extends Addon.
 export class TwitchChants extends Addon {
@@ -12,9 +15,6 @@ export class TwitchChants extends Addon {
 		this.inject('chat');
 		this.inject('site');
 		this.injectAs('site_chat', 'site.chat');
-		let current_chantmessage = "";
-		let current_chanttime = 0; 
-		let chant_delay = 25000;
 		let user = this.site.getUser();
 		
 
@@ -37,6 +37,9 @@ export class TwitchChants extends Addon {
 			process(tokens, msg) {
 				if (!msg.isHistorical) {
 					if (msg.badges.moderator || msg.badges.broadcaster) {
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////// leave for now for compatibility //////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 						if (msg.message.split(" ")[0] == "!chant") {
 							let current_chantmessage = msg.message.split("!chant")[1].trim();
 							if(current_chantmessage == "") {
@@ -55,6 +58,32 @@ export class TwitchChants extends Addon {
 								{
 									this.log.info ("chant on cooldown");
 									if (user.login == msg.user.login) { this.resolve('site.chat').addNotice(msg.roomLogin, "A chant is already running."); }
+								}
+							}
+						}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+						if (msg.message[0] == "⠀")
+						{
+							this.log.info("special character");
+							let current_chantmessage = msg.message.split("⠀")[1].trim()
+							this.log.info (msg.message);
+							this.log.info (current_chantmessage);
+							if(current_chantmessage == "") {
+								this.log.info("no chant message included");
+								//if (user.login == msg.user.login) { this.resolve('site.chat').addNotice(msg.roomLogin, "Correct usage of the command is: !chant <message>"); }
+							}
+							else {
+								let message_time = parseInt(msg.timestamp);
+								if (message_time > current_chanttime + chant_delay) {
+									let box = Chants.addChantButton( this, msg.user.login, current_chantmessage, msg.roomLogin);
+									document.getElementsByClassName("chat-input")[0].children[0].appendChild(box);
+									Chants.chantCooldown();
+									current_chanttime = message_time;
+								}
+								else
+								{
+									this.log.info ("chant on cooldown");
+									//if (user.login == msg.user.login) { this.resolve('site.chat').addNotice(msg.roomLogin, "A chant is already running."); }
 								}
 							}
 						}
@@ -89,7 +118,49 @@ export class TwitchChants extends Addon {
 
 		
 		this.createStyle();
-
+		
+		this.on('chat:get-tab-commands', e => {
+			this.log.info ("permission level: " + e.permissionLevel);
+			if ( e.permissionLevel >= 2 )
+			{
+				e.commands.push({
+					name: 'chant',
+					description: 'Send viewers a prompt for a hype message they can send to chat with one click',
+					permissionLevel: 2,
+					ffz_group: 'Chants',
+					commandArgs: [
+						{name: 'message', isRequired: true}
+					]
+				})
+			}
+        });
+		
+		this.on('chat:pre-send-message', e => {
+			const msg = e.message;
+			if (msg.split(" ")[0].toLowerCase() == "/chant") {
+                e.preventDefault();
+				let message = msg.slice(7).trim();
+				this.log.info ("chant message: " + message );
+				if (e.context._context.moderator)
+				{
+					this.log.info ("we are a mod");
+					if(message == "") {
+						this.log.info("no chant message included");
+						this.resolve('site.chat').addNotice(e.channel, "Correct usage of the command is: /chant <message>");
+					}
+					else
+					{
+						let tNow = new Date().getTime();
+						if (tNow > current_chanttime + chant_delay) {
+							this.resolve('site.chat').sendMessage(e.channel, "⠀" + message);
+							current_chanttime = tNow;
+						}
+						else { this.resolve('site.chat').addNotice(e.channel, "A chant is already running."); }
+					}
+				}
+				else { this.log.info ("not a mod"); this.resolve('site.chat').addNotice(e.channel, "Only moderators can start a chant"); }
+            }
+		});
 	}
 
 	// onDisable is called when the module should be disabled.
