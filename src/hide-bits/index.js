f7ec1a211f4f53ab1d4cdacaeac2762fc6e719c5
Gabriel Duarte <gabrielqduarte@gmail.com>
2022-08-02 04:02:35 +0200

    add the Hide Bits Addon
    The Addon hides the amount of bits in a chat message, especially useful to avoid spotting bit alerts that should be a surprise

---
+++ src/hide-bits/index.js
@@ -0,0 +1,5 @@
+const createElement = FrankerFaceZ.utilities.dom.createElement;
+
+var style = createElement("style");
+style.innerHTML = ".message .ffz-cheer {display: none}";
+document.head.append(style);
