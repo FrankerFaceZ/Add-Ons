e748fb0f8400ae1d0158ad8f766b80a271ea5603
Jonas Berner <EchtkPvL@users.noreply.github.com>
2022-08-02 23:04:55 +0200

    Emote Less Chat 1.0.5
    Initial release of new add-on that removes chat messages containing nothing but emotes.

---
+++ src/emoteless/index.js
@@ -0,0 +1,55 @@
+class EmoteLessChat extends Addon {
+
+	constructor(...args) {
+		super(...args);
+
+		this.inject('chat');
+
+		this.settings.add('emote_less_chat.enabled', {
+			default: true,
+
+			ui: {
+				path: 'Add-Ons > Emote Less Chat >> Enabled',
+				title: 'Enable filter',
+				description: 'Removes every chat message, that only contains emotes',
+				component: 'setting-check-box',
+			},
+		});
+
+		this.messageFilter = {
+			type: 'emote_less_chat',
+			priority: 9,
+
+			process(tokens, msg) {
+				if (!this.context.get('emote_less_chat.enabled')) return tokens;
+
+				let SPACES = /^\s+$/g;
+				let emoteOnly = true;
+				for(const token of tokens) {
+					if (token.type === 'emote') continue;
+					if (token.type === 'text' && SPACES.test(token.text)) continue;
+					emoteOnly = false;
+					break;
+				}
+
+				if(emoteOnly == false) return tokens;
+
+				msg.ffz_removed = true;
+
+				return tokens;
+			}
+		}
+	}
+
+	onEnable() {
+		this.chat.addTokenizer(this.messageFilter);
+		this.emit('chat:update-lines');
+	}
+
+	onDisable() {
+		this.chat.removeTokenizer(this.messageFilter);
+		this.emit('chat:update-lines');
+	}
+}
+
+EmoteLessChat.register();
