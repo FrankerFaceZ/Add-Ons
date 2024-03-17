db81b06cf3b78c6eddd22d44467f6f069af708a3
WizzzzardMedia <markus.krieger1@arcor.de>
2024-03-17 03:08:43 +0100

    Add files via upload

--- src/ragnarok-database/index.js
+++ src/ragnarok-database/index.js
@@ -1,16 +1,15 @@
 'use strict';
 
 const createElement = FrankerFaceZ.utilities.dom.createElement;
+const sanitize = FrankerFaceZ.utilities.dom.sanitize;
+
 import STYLE_RO from './styles.css';
 
 class RagnarokDatabase extends Addon {
 	constructor(...args) {
 		super(...args);
 
-		this.inject('chat');
-		
-		this.rodbCSS = null;
-		
+		this.inject('chat');				
 		this.settings.add('addon.rodb.server',{
 			default: 'iRO',
 			ui: {
@@ -42,57 +41,16 @@ class RagnarokDatabase extends Addon {
 		});
 		
 		var self = this;
-		
+
 		this.messageFilter = {
 			type: 'rodb',
 			priority: 9,
 
 			render(token, createElement){
-				if (!token.rodb)
-					return null;
-				if(token.rodb != "item"){
-					token.type = "link";
-					return (<a href={token.url} target="_newFrame">{token.url}</a>);
-				}
-
-				var xhr = self.fetchXHR(token.rodb, token.roid, self.server);
-
-				if(xhr == null){
-					if(self.server == 'kRO'){
-						token.type = "link";
-						return (<a href={token.url} target="_newFrame">{token.url}</a>);
-					}
-					
-					else if(self.server == 'iRO'){
-						xhr = self.fetchXHR(token.rodb, token.roid, 'kRO');
-						if(xhr == null){
-							token.type = "link";
-							return (<a href={token.url} target="_newFrame">{token.url}</a>);
-						}
-					}
-					
-					else{
-						xhr = self.fetchXHR(token.rodb, token.roid, 'iRO');
-						if(xhr == null){
-							xhr = self.fetchXHR(token.rodb, token.roid, 'kRO');
-							if(xhr == null){
-								token.type = "link";
-								return (<a href={token.url} target="_newFrame">{token.url}</a>);
-							}
-						}
-					}
-				}
-				console.log(xhr);
-				
-				xhr["description"] = xhr["description"].replaceAll('<script', '&lt;script');	//just to be save
-				xhr["description"] = xhr["description"].replaceAll(/(?:\r\n|\r|\n)/g, '<br>');
-				xhr["description"] = xhr["description"].replaceAll('^000000', '</font>');
-				xhr["description"] = xhr["description"].replaceAll(/\^([0-9a-fA-F]{6})/g, "<font color=\"#$1\">");
- 
 				return (
 					<div class="dp-tooltip dp-tooltip-item">
 						<div class="dp-tooltip-head">
-							<h3><a href={token.url} target="_newFrame">{xhr["name"]}</a></h3>
+							<h3><a href={token.url} target="_newFrame">{token.roname} ({token.roserver})</a></h3>
 						</div>
 						<div class="dp-tooltip-body">
 							<span class="dp-icon dp-icon-item dp-icon-item-large dp-icon-item-white">
@@ -101,7 +59,7 @@ class RagnarokDatabase extends Addon {
 							</span>
 							<div class="dp-item-properties">
 								<ul>
-									<li><font color="#000000" dangerouslySetInnerHTML={{ __html: xhr["description"] }}></font></li>
+									<li><font color="#000000" dangerouslySetInnerHTML={{ __html: token.description }}></font></li>
 								</ul>
 								<span class="clear"></span>
 							</div>
@@ -113,55 +71,84 @@ class RagnarokDatabase extends Addon {
 			process(tokens, msg) {
 				var regex = /.*divine-pride.net\/database\/([a-z]+)\/([0-9]+).*/;
 				for(const token of tokens) {
-					if (token.type === 'link' && regex.test(token.text)) {
+					if (token.type == 'link' && regex.test(token.text)) {
 						var parsed = regex.exec(token.text);
-						token.type = 'rodb';
-						token.rodb = parsed[1];
-						token.roid = parsed[2];
+						self.fetchXHR(parsed[1], parsed[2], self.server, token, msg);
 					}
 				}
 				return tokens;
 			}
 		}
 		
+		this.rodbCSS = null;
 		this.server = this.settings.get('addon.rodb.server');
 	}
 
 	onEnable() {
 		if(this.rodbCSS == null){
-			this.rodb = document.head.appendChild(createElement('link', {
+			this.rodbCSS = document.head.appendChild(createElement('link', {
 				href: STYLE_RO,
+				id: 'rodb-css',
 				rel: 'stylesheet',
 				type: 'text/css',
 				crossOrigin: 'anonymous'
 			}));
 		}
+		
 		this.log.info('Enabling support for divine-pride links.');
 		this.chat.addTokenizer(this.messageFilter);
 		this.emit('chat:update-lines');
 	}
 
 	onDisable() {
+		if(this.rodbCSS != null){
+			document.getElementById("rodb-css").remove();
+			this.rodbCSS = null;
+		}
 		this.chat.removeTokenizer(this.messageFilter);
 		this.emit('chat:update-lines');
 	}
 	
-	fetchXHR(from, id, srv){
+	async fetchXHR(from, id, server, token, message){
 		var xhr = new XMLHttpRequest();	
-		xhr.open("GET", "https://www.divine-pride.net/api/database/"+from+"/" + id + "?apiKey=92f0c44ff2bded0973a905cfc928cd66&server="+srv, false);
-		xhr.send(null);
+		xhr.open("GET", "https://www.divine-pride.net/api/database/"+from+"/" + id + "?apiKey=92f0c44ff2bded0973a905cfc928cd66&server="+server, true);
 		
-		if(xhr.status != 200)
-			return null;
-		
-		var parsed = JSON.parse(xhr.responseText);
-		
-		if(parsed["status"] != null)
-			return null;
-		
-		return parsed;
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState === XMLHttpRequest.DONE) {
+				const status = xhr.status;
+				if (status === 0 || (status >= 200 && status < 400)) {
+					var parsed = JSON.parse(xhr.responseText);					
+					if(parsed["status"] != null)
+						return;
+					
+					parsed["description"] = parsed["description"].replaceAll(/<a href.*?>/g, '');
+					parsed["description"] = parsed["description"].replaceAll('</a>', '');
+					parsed["description"] = sanitize(parsed["description"]);
+					parsed["description"] = parsed["description"].replaceAll(/(?:\r\n|\r|\n)/g, '<br>');
+					parsed["description"] = parsed["description"].replaceAll('^000000', '</font>');
+					parsed["description"] = parsed["description"].replaceAll(/\^([0-9a-fA-F]{6})/g, "<font color=\"#$1\">");
+					
+					token.type = "rodb";
+					token.roname = parsed["name"];
+					token.description = parsed["description"];
+					token.roid = id;
+					token.roserver = server;
+					
+					this.emit("chat:update-line", message.id, false);
+					
+				} else {
+					if(server == "kRO")
+						return
+					if(server == "iRO"){
+						this.fetchXHR(from, id, "kRO", token, message);
+						return;
+					}
+					this.fetchXHR(from, id, "iRO", token, message);					
+				}
+			}
+		};		
+		xhr.send();
 	}
-
 }
 
 RagnarokDatabase.register();
