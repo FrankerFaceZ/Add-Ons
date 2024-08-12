e68c322727f34b230e4aecc9e51d68a8fcb9e819
MrPandir <MrPandir@users.noreply.github.com>
2024-08-12 07:38:45 +0000

    (modchecker): use the new API to get badges

--- src/modchecker/index.js
+++ src/modchecker/index.js
@@ -3,89 +3,33 @@ class Modchecker extends Addon {
 		super(...args);
 		this.inject('chat.badges');
 
-		this.corsProxy = 'https://relaxo.dev/?a=';
+		const corsProxy = 'https://relaxo.dev/?a=';
 		this.domain = 'https://modchecker.com';
-		this.apiBase = `${this.corsProxy}${this.domain}/api/v1`;
+		this.apiBase = `${corsProxy}${this.domain}/api/v1`;
 		this.badgeBase = this.domain;
 
 		this.clickRedirectLink = this.domain;
 		this.namespace = 'addon.modchecker';
-		this.badgesInfo = [
-			{
-				textId: 'admin',
-				id: 11,
-				name: 'Modchecker Admin',
-				ffzSlot: 120,
-			},
-			{
-				textId: 'contributor',
-				id: 12,
-				name: 'Modchecker Contributor',
-				ffzSlot: 121,
-			},
-			{
-				textId: 'topdonator',
-				id: 10,
-				name: 'Modchecker Top Donator',
-				ffzSlot: 122,
-			},
-			{
-				textId: 'donator',
-				id: 8,
-				name: 'Modchecker Donator',
-				ffzSlot: 123,
-			},
-		];
-
 		this.badgeIds = new Set();
 	}
 
-	async onEnable() {
-		const promises = this.badgesInfo.map(badgeInfo => this.processBadge(badgeInfo));
-		await Promise.all(promises);
-		this.emit('chat:update-lines');
+	onEnable() {
+		this.loadBadges();
 	}
-
+	
 	onDisable() {
 		this.unloadBadges();
 	}
+	
+	async loadBadges() {
+		const badges = await this.fetchBadgeData();
 
-	async processBadge(badgeInfo) {
-		const data = await this.fetchBadgeData(badgeInfo.id);
-
-		const usersIds = data.map(item => item.id);
-		const firstBadgePath = data[0]?.badges[0]?.path;
-
-		if (!firstBadgePath) {
-			this.error(`Badge path not found for badge ${badgeInfo.textId}`);
-			return;
+		for (const badge of badges) {
+			const badgeId = this.registerBadge(badge);
+			this.badges.setBulk(this.namespace, badgeId, badge.users);
 		}
 
-		badgeInfo.url = `${this.badgeBase}${firstBadgePath}`;
-
-		const badgeId = this.registerBadge(badgeInfo);
-		this.badges.setBulk(this.namespace, badgeId, usersIds);
-	}
-
-	registerBadge(badge) {
-		const badgeId = `${this.namespace}.badge-${badge.textId}`;
-		this.badgeIds.add(badgeId);
-
-		this.badges.loadBadgeData(badgeId, {
-			id: badgeId,
-			name: badge.name,
-			title: badge.name,
-			click_url: this.clickRedirectLink,
-			image: badge.url,
-			urls: {
-				1: badge.url,
-				2: badge.url,
-				3: badge.url,
-			},
-			slot: badge.ffzSlot,
-		});
-
-		return badgeId;
+		this.emit('chat:update-lines');
 	}
 
 	unloadBadges() {
@@ -96,9 +40,39 @@ class Modchecker extends Addon {
 		this.emit('chat:update-lines');
 	}
 
-	fetchBadgeData(badgeId) {
-		const urlPath = `users?badgeId=${badgeId}`;
-		return this.request(urlPath);
+	createBadgeId(badgeName) {
+		// 1. translate into lower case
+		// 2. remove `modchecker` including spaces around
+		// 3. replace spaces with dashes
+		badgeName = badgeName.toLowerCase().replace(/\s*modchecker\s*/g, '').replace(/\s+/g, '-');
+
+		const badgeId = `${this.namespace}.badge-${badgeName}`;
+		this.badgeIds.add(badgeId);
+		return badgeId;
+	}
+
+	registerBadge(badge) {
+		const badgeId = this.createBadgeId(badge.name);
+
+		this.badges.loadBadgeData(badgeId, {
+			id: badgeId,
+			name: badge.name,
+			title: badge.name,
+			click_url: this.clickRedirectLink,
+			image: badge.path,
+			urls: {
+				1: badge.path,
+				2: badge.path,
+				3: badge.path,
+			},
+			slot: badge.ffzSlot,
+		});
+
+		return badgeId;
+	}
+
+	fetchBadgeData() {
+		return this.request('chatBadges');
 	}
 	
 	async request(path, defaultValue = []) {
