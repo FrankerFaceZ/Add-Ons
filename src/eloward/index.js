5e0c6ccf7be5ea599345cb4e01b320ae837cef26
SunnyWang0 <sunnywrocks@gmail.com>
2025-07-20 19:35:52 -0700

    diamond

--- src/eloward/index.js
+++ src/eloward/index.js
@@ -38,7 +38,7 @@ class EloWardFFZAddon extends FrankerFaceZ.utilities.addon.Addon {
 			gold: { width: '24px', height: '24px', margin: '0 0px 4px -4px'},
 			platinum: { width: '24px', height: '24px', margin: '0 0.5px 3.5px -3.5px'},
 			emerald: { width: '24px', height: '24px', margin: '0 1px 3px -3px'},
-			diamond: { width: '24px', height: '24px', margin: '0 2px 0 1px'},
+			diamond: { width: '24px', height: '24px', margin: '0 3px 4px -1px'},
 			master: { width: '24px', height: '24px', margin: '0 2px 0 1px'},
 			grandmaster: { width: '24px', height: '24px', margin: '0 2px 0 1px'},
 			challenger: { width: '24px', height: '24px', margin: '0 2px 0 1px'},
