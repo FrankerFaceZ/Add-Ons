756bbcb0289b83340c8da95b39b310b2f4b47ce2
ArgoWizbang <argowizbang@gmail.com>
2025-07-21 13:20:35 -0400

    Mass Moderation Utilities 2.1.1
    * Fixed: Importing recent followers by timestamp not working if a "Before" time was set with no "After" time set

--- src/mass-ban-tool/utils/date.js
+++ src/mass-ban-tool/utils/date.js
@@ -1,3 +1,11 @@
 export function isValidDate( dateObj ) {
     return dateObj instanceof Date && ! isNaN( dateObj );
 }
+
+export function getLocaleString( date ) {
+    if ( isValidDate( date ) ) {
+        return date.toLocaleString();
+    } else {
+        return '---';
+    }
+}
