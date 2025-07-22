7c63571a0d5a743f0f5a8fa99ea48193123bedde
SunnyWang0 <sunnywrocks@gmail.com>
2025-07-21 22:44:24 -0700

    asdf

--- src/eloward/index.js
+++ src/eloward/index.js
@@ -56,7 +56,7 @@ class EloWardFFZAddon extends FrankerFaceZ.utilities.addon.Addon {
 			iron: { width: '28px', height: '28px', margin: '0px -7px 5px -3px'},
 			bronze: { width: '26px', height: '26px', margin: '0px -7px 3px -3px'},	
 			silver: { width: '24px', height: '24px', margin: '0px -7px 2px -3px'},
-			gold: { width: '24px', height: '24px', margin: '0px -7px 2px -3px'},
+			gold: { width: '24px', height: '24px', margin: '0px -7px 0px -3px'},
 			platinum: { width: '24px', height: '24px', margin: '0px -7px 5px -3px'},
 			emerald: { width: '24px', height: '24px', margin: '0px -7px 5px -3px'},
 			diamond: { width: '24px', height: '24px', margin: '0px -7px 5px -3px'},
