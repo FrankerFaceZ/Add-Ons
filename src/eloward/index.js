243fec2b74a16da1c4c519839a5fbc8b90775cc0
SunnyWang0 <sunnywrocks@gmail.com>
2025-07-21 22:45:44 -0700

    platbun

--- src/eloward/index.js
+++ src/eloward/index.js
@@ -57,7 +57,7 @@ class EloWardFFZAddon extends FrankerFaceZ.utilities.addon.Addon {
 			bronze: { width: '26px', height: '26px', margin: '0px -7px 3px -3px'},	
 			silver: { width: '24px', height: '24px', margin: '0px -7px 2px -3px'},
 			gold: { width: '24px', height: '24px', margin: '0px -7px 0px -3px'},
-			platinum: { width: '24px', height: '24px', margin: '0px -7px 5px -3px'},
+			platinum: { width: '24px', height: '24px', margin: '2px -5px 0px -1px'},
 			emerald: { width: '24px', height: '24px', margin: '0px -7px 5px -3px'},
 			diamond: { width: '24px', height: '24px', margin: '0px -7px 5px -3px'},
 			master: { width: '24px', height: '24px', margin: '0px -7px 5px -3px'},
